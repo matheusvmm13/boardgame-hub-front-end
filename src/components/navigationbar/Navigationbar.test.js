cb4e9254d9dfa93a5a2a0b8d61fedf48f27c68d4
Matheus Martins <matheusvmm13@gmail.com>
2022-03-08 10:00:42 +0100

    Created test for the navigationbar component

---
+++ src/components/navigationbar/Navigationbar.test.js
@@ -0,0 +1,24 @@
+import { render, screen } from "@testing-library/react";
+import Navigationbar from "./Navigationbar";
+
+describe("Given a Navigationbar component", () => {
+  describe("When it get's rendered", () => {
+    test("Then it should display a navigation with six links", () => {
+      render(<Navigationbar />);
+
+      const firstNavLink = screen.getByRole("link", { name: /All Matches/i });
+      const secondNavLink = screen.getByRole("link", { name: /My Matches/i });
+      const thirdNavLink = screen.getByRole("link", { name: /New Match/i });
+      const forthNavLink = screen.getByRole("link", { name: /My Boardgames/i });
+      const fifthNavLink = screen.getByRole("link", { name: /Login/i });
+      const sixthNavLink = screen.getByRole("link", { name: /Sign Up/i });
+
+      expect(firstNavLink).toBeInTheDocument();
+      expect(secondNavLink).toBeInTheDocument();
+      expect(thirdNavLink).toBeInTheDocument();
+      expect(forthNavLink).toBeInTheDocument();
+      expect(fifthNavLink).toBeInTheDocument();
+      expect(sixthNavLink).toBeInTheDocument();
+    });
+  });
+});
