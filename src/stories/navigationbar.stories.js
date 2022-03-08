a97dbeb0a8714a582457c3048fd7814731ec4776
Matheus Martins <matheusvmm13@gmail.com>
2022-03-08 17:33:19 +0100

    Added navbar story from storybook

---
+++ src/stories/navigationbar.stories.js
@@ -0,0 +1,9 @@
+import React from "react";
+import Navigationbar from "../components/navigationbar/Navigationbar";
+
+export default {
+  title: "Navbar",
+  component: Navigationbar,
+};
+
+export const Responsive = () => <Navigationbar />;
