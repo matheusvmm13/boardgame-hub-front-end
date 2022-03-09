6ff1f9aac6922ceb07cac93e7a9648e1e15360e1
Matheus Martins <matheusvmm13@gmail.com>
2022-03-09 11:53:05 +0100

    Fixed reducer's test

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -2,11 +2,15 @@ import React from "react";
 import { render, screen } from "@testing-library/react";
 import App from "./App";
 import { BrowserRouter } from "react-router-dom";
+import { Provider } from "react-redux";
+import store from "./redux/store";
 
-test("renders learn react link", () => {
+test("renders the page title", () => {
   render(
     <BrowserRouter>
-      <App />
+      <Provider store={store}>
+        <App />
+      </Provider>
     </BrowserRouter>
   );
   const linkElement = screen.getAllByText(/All Matches/i);
--- src/redux/reducers/matchReducers.test.ts
+++ src/redux/reducers/matchReducers.test.ts
@@ -1,10 +1,11 @@
+import { MatchInterface } from "../../utils/types/matchInterface";
 import actionsType from "../actions/actionsType";
 import matchReducer from "./matchReducer";
 
 describe("Given a matchReducer function", () => {
   describe("When it is called with an empty state and the loadMatches action with an match", () => {
     test("Then it should return the new state with the array of matches", () => {
-      const currentState: [] = [];
+      const state: MatchInterface[] = [];
 
       const matchesArray = [
         {
@@ -13,16 +14,27 @@ describe("Given a matchReducer function", () => {
           image: "",
           creator: "username",
           date: "april 1990",
+          maxPlayers: 5,
+          players: ["", "", ""],
+          location: "Madrid",
+        },
+        {
+          id: "531452",
+          gameTitle: "Carcassone",
+          image: "",
+          creator: "username2",
+          date: "march 2020",
+          maxPlayers: 4,
           players: ["", "", ""],
           location: "Barcelona",
         },
       ];
       const action = {
         type: actionsType.loadMatches,
-        matches: matchesArray,
+        match: matchesArray,
       };
 
-      const newState = matchReducer(currentState, action);
+      const newState = matchReducer(state, action);
 
       expect(newState).toEqual(matchesArray);
     });
