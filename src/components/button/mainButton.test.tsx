d867a6fe0f4ccf0a544e56248944278ae11687eb
Matheus Martins <matheusvmm13@gmail.com>
2022-03-09 19:27:58 +0100

    Added button test

---
+++ src/components/button/mainButton.test.tsx
@@ -0,0 +1,16 @@
+import { render, screen } from "@testing-library/react";
+import MainButton from "./MainButton";
+
+describe("Given a main button component", () => {
+  describe("When it's instanciated", () => {
+    test("Then it should render the given text", () => {
+      const buttonText = "Click Me";
+
+      render(<MainButton buttonText={buttonText} />);
+
+      const button = screen.getByRole("button", { name: buttonText });
+
+      expect(button).toBeInTheDocument();
+    });
+  });
+});
