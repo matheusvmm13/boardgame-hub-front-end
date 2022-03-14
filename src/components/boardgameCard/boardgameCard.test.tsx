2a85d5f14fdaa91d5b7d688cc0fa67a1e44fd245
Matheus Martins <matheusvmm13@gmail.com>
2022-03-14 17:00:49 +0100

    Fixed Sonar code smells

--- src/components/boardgameCard/boardgameCard.test.tsx
+++ src/components/boardgameCard/boardgameCard.test.tsx
@@ -1,7 +1,7 @@
 import { render, screen } from "@testing-library/react";
 import { Provider } from "react-redux";
 import store from "../../redux/store";
-import BoardgameCard from "./BoardgameCardComponent";
+import BoardgameCard from "./BoardgameCard";
 
 describe("Given a Match Card component", () => {
   describe("When it's rendered", () => {
