e6d7462a90b5787e7ddda218a41ae12264276206
Matheus Martins <matheusvmm13@gmail.com>
2022-03-15 13:47:42 +0100

    Populated the My Boardgames page for loged users

--- src/pages/my-boardgames/MyBoardgamesPage.tsx
+++ src/pages/my-boardgames/MyBoardgamesPage.tsx
@@ -5,38 +5,50 @@ import jwtDecode from "jwt-decode";
 import { DecodedToken } from "../../components/form/FormMatch";
 import Header from "../../components/header/Header";
 import { RootState } from "../../redux/reducers";
-import { loadMyMatchesThunk } from "../../redux/thunks/matchThunk";
+import { loadMyGamesThunk } from "../../redux/thunks/boardgameThunks";
 import BoardgameCard from "../../components/boardgameCard/BoardgameCard";
 
-const MyMatchesPage = () => {
+const MyBoardgamesPage = () => {
   const dispatch = useDispatch();
-  const myGamesData = useSelector((state: RootState) => state.games);
+  const myGamesData = useSelector((state: RootState) => state.boardGames);
 
   useEffect(() => {
     const token: string | null = localStorage.getItem("token");
     if (token !== null) {
       const decodedToken: DecodedToken = jwtDecode(token);
       const userId = decodedToken.id;
-      dispatch(loadMyMatchesThunk(userId));
+      dispatch(loadMyGamesThunk(userId));
     }
   }, [dispatch]);
 
   return (
     <>
       <Wrapper className="container">
-        <Header title={"My Matches"} />
-        <ul className="matches__list"></ul>
+        <Header title={"My Boardgames"} />
+        <ul className="boardgames__list">
+          {myGamesData.map((game) => (
+            <BoardgameCard
+              name={game.name}
+              image_url={game.image_url}
+              min_players={game.min_players}
+              max_players={game.max_players}
+              max_playtime={game.max_playtime}
+              key={game.name}
+              _id={game._id}
+            />
+          ))}
+        </ul>
       </Wrapper>
     </>
   );
 };
 
-export default MyMatchesPage;
+export default MyBoardgamesPage;
 
 const Wrapper = styled.section`
   min-height: 100vh;
 
-  .matches__list {
+  .boardgames__list {
     display: flex;
     flex-wrap: wrap;
     justify-content: space-around;
